5f0d22a0584b7870a8ed9cffffb5f7510ec9c58d
Paolo La Camera <paolo@parity.io>
2025-06-30 15:45:26 +0200

    Remove unneeded WeightInfo trait bounds

--- substrate/frame/fast-unstake/src/mock.rs
+++ substrate/frame/fast-unstake/src/mock.rs
@@ -153,7 +153,6 @@ impl pallet_staking_async::Config for Runtime {
 	type CurrencyToVote = SaturatingCurrencyToVote;
 	type EraPayout = EraPayout;
 	type RcClientInterface = ();
-	type WeightInfo = ();
 }
 
 parameter_types! {
