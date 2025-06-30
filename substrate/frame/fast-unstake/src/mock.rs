502139d1034abdf60bf8b455b807505156147e77
Paolo La Camera <paolo@parity.io>
2025-06-30 15:26:08 +0200

    Replace mock RcClient with unit type for tests

--- substrate/frame/fast-unstake/src/mock.rs
+++ substrate/frame/fast-unstake/src/mock.rs
@@ -137,8 +137,7 @@ impl pallet_staking_async::EraPayout<Balance> for MockEraPayout {
 }
 
 // Mock RC client interface
-pub struct MockRcClient;
-impl rc_client::RcClientInterface for MockRcClient {
+impl rc_client::RcClientInterface for () {
 	type AccountId = AccountId;
 	fn validator_set(
 		_new_validator_set: Vec<Self::AccountId>,
@@ -158,7 +157,7 @@ impl pallet_staking_async::Config for Runtime {
 	type TargetList = UseValidatorsMap<Self>;
 	type CurrencyToVote = SaturatingCurrencyToVote;
 	type EraPayout = MockEraPayout;
-	type RcClientInterface = MockRcClient;
+	type RcClientInterface = ();
 	type WeightInfo = ();
 }
 
