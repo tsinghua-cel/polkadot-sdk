9f9a2652bdd8605b1977abf999a326d776b32b0b
github-action-benchmark <github@users.noreply.github.com>
2025-07-01 18:36:12 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 6824a6782354e274cf308be314f142551c5457ab

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751386184947,
+  "lastUpdate": 1751394971225,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -52443,6 +52443,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04528547773799997,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1431590+nkpar@users.noreply.github.com",
+            "name": "Nikolai Kozlov",
+            "username": "nkpar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "6824a6782354e274cf308be314f142551c5457ab",
+          "message": "Add optional auto-rebag within on-idle (#8684)\n\nImplemented an `on_idle` hook for automatic rebagging of accounts,\nenabling incremental correction of account positions within the\nbags-list during the idle phase of block execution. Introduced new\nstorages and configurable constants to support this functionality, along\nwith improvements in logging and internal helpers.\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>\nCo-authored-by: Ankan <10196091+Ank4n@users.noreply.github.com>\nCo-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>",
+          "timestamp": "2025-07-01T17:34:56Z",
+          "tree_id": "d7d7c3d9377a77baccd6e32ad81ac989e50d8655",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/6824a6782354e274cf308be314f142551c5457ab"
+        },
+        "date": 1751394954562,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.93599999999995,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.044905160331999976,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.034039890816,
             "unit": "seconds"
           }
         ]
