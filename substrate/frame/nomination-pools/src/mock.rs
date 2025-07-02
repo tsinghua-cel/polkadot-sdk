18d03712dca975dc51cb708573483f2e9d7211a0
Paolo La Camera <paolo@parity.io>
2025-07-02 10:16:10 +0200

    Fix nomination pools

--- substrate/frame/nomination-pools/src/mock.rs
+++ substrate/frame/nomination-pools/src/mock.rs
@@ -222,7 +222,7 @@ impl sp_staking::StakingInterface for StakingMock {
 		unimplemented!("method currently not used in testing")
 	}
 
-	#[cfg(feature = "runtime-benchmarks")]
+	#[cfg(any(test, feature = "runtime-benchmarks"))]
 	fn add_era_stakers(
 		_current_era: &EraIndex,
 		_stash: &Self::AccountId,
@@ -231,12 +231,12 @@ impl sp_staking::StakingInterface for StakingMock {
 		unimplemented!("method currently not used in testing")
 	}
 
-	#[cfg(feature = "runtime-benchmarks")]
+	#[cfg(any(test, feature = "runtime-benchmarks"))]
 	fn set_current_era(_era: EraIndex) {
 		unimplemented!("method currently not used in testing")
 	}
 
-	#[cfg(feature = "runtime-benchmarks")]
+	#[cfg(any(test, feature = "runtime-benchmarks"))]
 	fn max_exposure_page_size() -> sp_staking::Page {
 		unimplemented!("method currently not used in testing")
 	}
@@ -410,7 +410,7 @@ impl DelegationMigrator for DelegateMock {
 		unimplemented!("not used in current unit tests")
 	}
 
-	#[cfg(feature = "runtime-benchmarks")]
+	#[cfg(any(test, feature = "runtime-benchmarks"))]
 	fn force_kill_agent(_agent: Agent<Self::AccountId>) {
 		unimplemented!("not used in current unit tests")
 	}
