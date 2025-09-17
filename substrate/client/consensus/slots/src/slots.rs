e5a1511b78c8c9a6d480379adc6d2485abb37f80
luxq <xueqian1991@gmail.com>
2025-09-17 02:04:30 +0000

    fix bug

--- substrate/client/consensus/slots/src/slots.rs
+++ substrate/client/consensus/slots/src/slots.rs
@@ -143,7 +143,12 @@ where
 			// Schedule delay for next slot.
 			log::debug!(target: LOG_TARGET, "next slot internal trigger.");
 			let mut wait_dur = time_until_next_slot(self.slot_duration);
-			wait_dur = wait_dur.sub(Duration::from_millis(3000));
+			// check wait_dur is bigger than 3 seconds to avoid underflow
+			if wait_dur > Duration::from_millis(3000) {
+				wait_dur = wait_dur.sub(Duration::from_millis(3000));
+			} else {
+				wait_dur = Duration::from_millis(100);
+			}
 
 			self.until_next_slot = Some(Delay::new(wait_dur));
 
