37ed275beab486beb6d4073244d62ecdc64b83b4
luxq <xueqian1991@gmail.com>
2025-09-21 10:18:49 +0800

    update branch

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # build binary
 cargo build --release
 # build and publish docker image
-./docker/scripts/build-injected.sh && docker tag parity/polkadot:latest tscel/polkadot:exp-1.16.9 && docker push tscel/polkadot:exp-1.16.9
+./docker/scripts/build-injected.sh && docker tag parity/polkadot:latest tscel/polkadot:exp-delay-1.16.9 && docker push tscel/polkadot:exp-delay-1.16.9
